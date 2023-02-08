a2cac4320ad8e89b2c3950d1ce92f1d15a08bfec
Justin Hiltz <justin@dumbstuff.net>
2023-02-08 15:19:24 -0500

    Fixing all of Michelle's mistakes, as usual

--- client/src/components/MemeList.js
+++ client/src/components/MemeList.js
@@ -28,36 +28,6 @@ const MemesListPage = (props) => {
     return <MemeTile key={memeObject.id} {...memeObject} />;
   });
 
-  // const handleImageUpload = (acceptedImage) => {
-  //   setNewMemeFormData({
-  //     ...newMemeFormData,
-  //     image: acceptedImage[0],
-  //   });
-  // };
-  // const addMeme = async (event) => {
-  //   event.preventDefault();
-  //   const newMemeBody = new FormData();
-  //   newMemeBody.append("title", newMemeFormData.title);
-  //   newMemeBody.append("image", newMemeFormData.image);
-
-  //   try {
-  //     const response = await fetch("/api/v1/memes", {
-  //       method: "POST",
-  //       headers: {
-  //         Accept: "image/jpeg",
-  //       },
-  //       body: newMemeBody,
-  //     });
-  //     if (!response.ok) {
-  //       throw new Error(`${response.status} (${response.statusText})`);
-  //     }
-  //     const body = await response.json();
-  //     setMemes([...memes, body.meme]);
-  //   } catch (error) {
-  //     console.error(`Error in addMeme Fetch: ${error.message}`);
-  //   }
-  // };
-
   return (
     <div className="grid-container">
       <div className="grid-x grid-margin-x grid-margin-y align-center">

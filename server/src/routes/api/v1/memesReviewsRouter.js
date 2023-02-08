2874285716d3014ae682d38cc9d73cc1e4a84c69
Nyck Jean-Philippe <nyckjeanphilippe@gmail.com>
2023-02-08 17:03:12 -0500

    .

--- server/src/routes/api/v1/memesReviewsRouter.js
+++ server/src/routes/api/v1/memesReviewsRouter.js
@@ -17,7 +17,6 @@ memesReviewsRouter.post("/", async (req, res) => {
     const newReview = await Review.query().insertAndFetch({ rating, content, memeId, userId });
     return res.status(201).json({ review: newReview });
   } catch (error) {
-    console.log(error);
     if (error instanceof ValidationError) {
       return res.status(422).json({ errors: error.data });
     }
